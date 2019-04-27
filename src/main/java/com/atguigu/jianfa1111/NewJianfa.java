159cb8b864dba117b7b24384315b57e5deda9774
zhangchen <windyzj@163.com>
2019-04-27 15:30:17 +0800

    new剑法

--- src/main/java/com/atguigu/jianfa1111/NewJianfa.java
+++ src/main/java/com/atguigu/jianfa1111/NewJianfa.java
@@ -4,5 +4,8 @@ public class NewJianfa {
 
     public static void main(String[] args) {
         System.out.println("new JIanfa 11111");
+        System.out.println("new JIanfa 2222");
+        System.out.println("new JIanfa 222");
+        System.out.println("new JIanfa 2222");
     }
 }
